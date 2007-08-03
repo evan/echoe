2b9d2ea1fb519e54f9c6f6e014e20d772b0fa616
Evan Weaver <evan@cloudbur.st>
2007-08-03 05:05:20 +0000

    update docs

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ require 'lib/echoe'
 Echoe.new('echoe') do |p|
   p.rubyforge_name = "fauna"
   p.author = "Evan Weaver"
-  p.summary = "Echoe is a simple packaging tool for Ruby gems."
+  p.summary = "A tool for packaging Ruby gems."
   p.url = "http://blog.evanweaver.com/pages/code#echoe"
   p.docs_host = "blog.evanweaver.com:~/www/snax/public/files/doc/"
   p.extra_deps = ['rake', 'rubyforge >= 0.4.0', 'highline']
--- lib/echoe.rb
+++ lib/echoe.rb
@@ -19,7 +19,7 @@ For example, Echoe's own <tt>Rakefile</tt> looks like this:
   Echoe.new('echoe') do |p|
     p.rubyforge_name = "fauna"
     p.author = "Evan Weaver"
-    p.summary = "Echoe is a simple tool for packaging Rubygems."
+    p.summary = "A tool for packaging Ruby gems."
     p.url = "http://blog.evanweaver.com/pages/code#echoe"
     p.docs_host = "blog.evanweaver.com:~/www/snax/public/files/doc/"
     p.extra_deps = ['rake', 'rubyforge >= 0.4.0', 'highline']
