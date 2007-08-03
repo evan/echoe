1594d13566eee57af92da79b74a5e597a1d75bdc
Evan Weaver <evan@cloudbur.st>
2007-08-03 03:08:33 +0000

    hmm... what broke?

--- Rakefile
+++ Rakefile
@@ -1,19 +1,16 @@
 
 require 'lib/echoe'
 
-Echoe.new("echoe", `cat CHANGELOG`[/^([\d\.]+)\. /, 1]) do |p|
-  p.name = "echoe"
+Echoe.new('echoe') do |p|
   p.rubyforge_name = "fauna"
   p.author = "Evan Weaver"
   p.summary = "Echoe is a simple packaging tool for working with rubygems."
   p.description = "Echoe is a simple packaging tool for working with rubygems. It
 generates all the usual tasks for projects including rdoc generation, testing, packaging, and deployment."
-  p.changes = `cat CHANGELOG`[/^([\d\.]+\. .*)/, 1]
 
   p.url = "http://blog.evanweaver.com/pages/code#echoe"
   p.docs_host = "blog.evanweaver.com:~/www/snax/public/files/doc/"
 
-  p.extra_deps = [['rake'], ['rubyforge', '>= 0.4.0']]
-
+  p.extra_deps = ['rake', 'rubyforge >= 0.4.0', 'highline']
 end
 
--- lib/echoe.rb
+++ lib/echoe.rb
@@ -1,4 +1,3 @@
-# -*- ruby -*-
 
 require 'rubygems'
 require 'rake'
@@ -7,88 +6,9 @@ require 'rake/gempackagetask'
 require 'rake/rdoctask'
 require 'rake/testtask'
 require 'rbconfig'
-
-##
-# echoe - a tool to help rake
-#
-# echoe is a simple rake/rubygems helper for project Rakefiles. It
-# generates all the usual tasks for projects including rdoc generation,
-# testing, packaging, and deployment.
-#
-# == Using echoe
-#
-# === Basics
-#
-# Use this as a minimal starting point:
-#
-#   require 'echoe'
-# 
-#   Echoe.new("project_name", '1.0.0') do |p|
-#     p.rubyforge_name = "rf_project"
-#     # add other details here
-#   end
-#
-#   # add other tasks here
-#
-# === Tasks Provided:
-# 
-# * announce         - Generate email announcement file and post to rubyforge.
-# * audit            - Run ZenTest against the package
-# * check_manifest   - Verify the manifest
-# * build_manifest   - Build a manifest from your current tree
-# * clean            - Clean up all the extras
-# * debug_gem        - Show information about the gem.
-# * default          - Run the default tasks
-# * docs             - Build the docs HTML Files
-# * email            - Generate email announcement file.
-# * install          - Install the package. Uses PREFIX and RUBYLIB
-# * install_gem      - Install the package as a gem
-# * multi            - Run the test suite using multiruby
-# * package          - Build all the packages
-# * post_news        - Post announcement to rubyforge.
-# * publish_docs     - Publish RDoc to RubyForge
-# * release          - Package and upload the release to rubyforge.
-# * ridocs           - Generate ri locally for testing
-# * test             - Run the test suite. Use FILTER to add to the command line.
-# * test_deps        - Show which test files fail when run alone.
-# * uninstall        - Uninstall the package.
-#
-# === Attributes
-#
-# The attributes that you can provide inside the new block above are:
-#
-# ==== Mandatory
-#
-# * name        - The name of the release.
-# * version     - The version. Don't hardcode! use a constant in the project.
-#
-# ==== Good to Set
-#
-# * author      - The author of the package.
-# * changes     - A description of the release's latest changes.
-# * description - A description of the project.
-# * email       - The author's email address.
-# * summary     - A short summary of the project.
-# * url         - The url of the project.
-#
-# ==== Optional
-#
-# * clean_globs    - An array of file patterns to delete on clean.
-# * extra_deps     - An array of rubygem dependencies.
-# * need_tar       - Should package create a tarball? [default: true]
-# * need_zip       - Should package create a zipfile? [default: false]
-# * rdoc_pattern   - A regexp to match documentation files against the manifest.
-# * rdoc_template  - Path to an external rdoc template file
-# * rubyforge_name - The name of the rubyforge project. [default: name.downcase]
-# * spec_extras    - A hash of extra values to set in the gemspec.
-# * test_globs     - An array of test file patterns [default: test/**/test_*.rb]
-#
-# === Environment Variables
-#
-# * FILTER     - Used to add flags to test_unit (e.g., -n test_borked)
-# * PREFIX     - Used to specify a custom install location (for rake install).
-# * RUBY_DEBUG - Used to add extra flags to RUBY_FLAGS.
-# * RUBY_FLAGS - Used to specify flags to ruby [has smart default].
+require 'highline/import'
+gem 'rubyforge', '>= 0.4.0'
+require 'rubyforge'
 
 class Echoe
 
@@ -106,30 +26,40 @@ class Echoe
     "-w -I#{%w(lib ext bin test).join(File::PATH_SEPARATOR)}" +
     (RUBY_DEBUG ? " #{RUBY_DEBUG}" : '')
   FILTER = ENV['FILTER'] # for tests (eg FILTER="-n test_blah")
-  attr_accessor :author, :bin_files, :changes, :clean_globs, :description, :email, :extra_deps, :lib_files, :name, :need_tar, :need_tar_gz, :need_zip, :rdoc_pattern, :rubyforge_name, :spec, :spec_extras, :summary, :test_files, :test_globs, :url, :version, :tar_extension, :docs_host, :rdoc_template #, :tar_separator, :zip_separator
-
-  def initialize(name, version)
+  attr_accessor :author, :changes, :clean_pattern, :description, :email, :extra_deps, :name, :need_tar, :need_tar_gz, :need_zip, :rdoc_pattern, :rubyforge_name, :summary, :test_patterns, :url, :version, :docs_host, :rdoc_template 
+  
+  def initialize(name, version = nil)
     self.name = name
-    self.version = version
+    
+    self.version = if version
+      version
+    elsif File.exist? "CHANGELOG"
+      open("CHANGELOG").read[/^v([\d\.]+)\. /, 1]
+    else
+      raise "No version supplied in Rakefile"
+    end
 
     # Defaults
     self.rubyforge_name = name.downcase
     self.url = ""
     self.author = ""
     self.email = ""
-    self.clean_globs = %w(diff diff.txt email.txt ri *.gem **/*~)
-    self.test_globs = ['test/**/test_*.rb']
-    self.changes = ""
+    self.clean_pattern = %w(diff diff.txt email.txt ri *.gem **/*~)
+    self.test_patterns = ['test/**/test_*.rb']
+    
+    self.changes = if File.exist? "CHANGELOG"
+      open("CHANGELOG").read[/^v([\d\.]+\. .*)/, 1]
+    else
+      ""
+    end
+    
     self.description = ""
     self.summary = ""
     self.rdoc_pattern = /^(\.\/|)(lib|bin|tasks)|README|CHANGELOG|LICENSE|txt$/
     self.extra_deps = []
-    self.spec_extras = {}
-    self.need_tar = true
-    self.need_tar_gz = false
+    self.need_tar = false
+    self.need_tar_gz = true
     self.need_zip = false
-#    self.tar_separator = "." # separator choice is buried in rake/packagetask, bah
-#    self.zip_separator = "."
 
     yield self if block_given?
 
@@ -145,22 +75,6 @@ class Echoe
       run_tests
     end
 
-    desc 'Show which test files fail when run alone.'
-    task :test_deps do
-      tests = Dir["test/**/test_*.rb"]  +  Dir["test/**/*_test.rb"]
-
-      tests.each do |test|
-        if not system "ruby -Ibin:lib:test #{test} &> /dev/null" then
-          puts "Dependency Issues: #{test}"
-        end
-      end
-    end
-
-    desc 'Run the test suite using multiruby'
-    task :multi do
-      run_tests :multi
-    end
-
     ############################################################
     # Packaging and Installing
 
@@ -170,12 +84,13 @@ class Echoe
       s.summary = summary
       s.author = Array(author).join(", ")
       s.email = email
-      s.homepage = Array(url).first
+      s.homepage = url
       s.rubyforge_project = rubyforge_name
 
       s.description = description
 
       extra_deps.each do |dep|
+        dep = dep.split(" ") if dep.is_a? String
         s.add_dependency(*dep)
       end
 
@@ -195,18 +110,9 @@ class Echoe
       if File.exist? "test/test_all.rb"
         s.test_file = "test/test_all.rb"
       else
-        s.test_files = Dir[*test_globs]
+        s.test_files = Dir[*test_patterns]
       end
 
-      # Do any extra stuff the user wants
-      spec_extras.each do |msg, val|
-        case val
-        when Proc
-          val.call(s.send(msg))
-        else
-          s.send "#{msg}=", val
-        end
-      end
     end
 
     desc 'Show information about the gem.'
@@ -239,47 +145,45 @@ class Echoe
       end
     end
 
-    desc 'Package and upload the release to rubyforge.'
-    task :release => [:clean, :package] do |t|
-      v = ENV["VERSION"] or abort "Must supply VERSION=x"
-      abort "Versions don't match #{v} vs #{version}" if v != version
+    desc 'Package and upload the release to Rubyforge.'
+    task :release => [:clean, :package] do |t|      
 
-      gem 'rubyforge', '>= 0.4.0'
-      require 'rubyforge'
-
-      pkg = "pkg/#{name}-#{version}"
-      pkg_gem = pkg + ".gem"
-      pkg_tar = pkg + ".tgz"
-      pkg_tar_gz = pkg + ".tar.gz"
-      pkg_zip = pkg + ".zip" 
+      if ask "Release version #{version} to Rubyforge?"      
+        pkg = "pkg/#{name}-#{version}"
+        pkg_gem = pkg + ".gem"
+        pkg_tar = pkg + ".tgz"
+        pkg_tar_gz = pkg + ".tar.gz"
+        pkg_zip = pkg + ".zip" 
+        
+        if $DEBUG then
+          puts "release_id = rf.add_release #{rubyforge_name.inspect}, #{name.inspect}, #{version.inspect}, \"#{pkg_tar}\""
+          puts "rf.add_file #{rubyforge_name.inspect}, #{name.inspect}, release_id, \"#{pkg_gem}\""
+        end
+  
+        rf = RubyForge.new
+        puts "Logging in"
+        rf.login
+  
+        c = rf.userconfig
+        c["release_notes"] = description if description
+        c["release_changes"] = changes if changes
+        c["preformatted"] = false
+  
+        files = [(@need_tar ? pkg_tar : nil),
+                  (@need_tar_gz ? pkg_tar_gz : nil),
+                 (@need_zip ? pkg_zip : nil),
+                 pkg_gem].compact
+  
+        puts "Releasing #{name} v. #{version}"
+        self.version = self.version.ljust(3)
+  
+        begin
+          rf.add_release rubyforge_name, name, version, *files
+        rescue NoMethodError => boom
+         # be silent
+        end
+      end
       
-      if $DEBUG then
-        puts "release_id = rf.add_release #{rubyforge_name.inspect}, #{name.inspect}, #{version.inspect}, \"#{pkg_tar}\""
-        puts "rf.add_file #{rubyforge_name.inspect}, #{name.inspect}, release_id, \"#{pkg_gem}\""
-      end
-
-      rf = RubyForge.new
-      puts "Logging in"
-      rf.login
-
-      c = rf.userconfig
-      c["release_notes"] = description if description
-      c["release_changes"] = changes if changes
-      c["preformatted"] = false
-
-      files = [(@need_tar ? pkg_tar : nil),
-                (@need_tar_gz ? pkg_tar_gz : nil),
-               (@need_zip ? pkg_zip : nil),
-               pkg_gem].compact
-
-      puts "Releasing #{name} v. #{version}"
-      self.version = self.version.ljust(3)
-
-      begin
-        rf.add_release rubyforge_name, name, version, *files
-      rescue NoMethodError => boom
-       # be silent
-      end
     end
 
     ############################################################
@@ -353,7 +257,7 @@ class Echoe
  
     desc 'Clean up all the extras'
     task :clean => [ :clobber_docs, :clobber_package ] do
-      clean_globs.each do |pattern|
+      clean_pattern.each do |pattern|
         files = Dir[pattern]
         rm_rf files unless files.empty?
       end
@@ -401,7 +305,7 @@ class Echoe
     cmd = if test ?f, 'test/test_all.rb' then
             "#{RUBY_FLAGS} test/test_all.rb #{FILTER}"
           else
-            tests = test_globs.map { |g| Dir.glob(g) }.flatten << 'test/unit'
+            tests = test_patterns.map { |g| Dir.glob(g) }.flatten << 'test/unit'
             tests.map! {|f| %Q(require "#{f}")}
             "#{RUBY_FLAGS} -e '#{tests.join("; ")}' #{FILTER}"
           end
