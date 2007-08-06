c870b40d9dca46788f55065327709327a997f748
Evan Weaver <evan@cloudbur.st>
2007-08-06 20:10:59 +0000

    docs

--- Rakefile
+++ Rakefile
@@ -2,11 +2,11 @@
 require 'lib/echoe'
 
 Echoe.new('echoe') do |p|
-  p.project = "fauna"
-  p.author = "Evan Weaver"
-  p.summary = "A tool for packaging Ruby gems."
-  p.url = "http://blog.evanweaver.com/pages/code#echoe"
-  p.docs_host = "blog.evanweaver.com:~/www/snax/public/files/doc/"
+  p.project = 'fauna'
+  p.author = 'Evan Weaver'
+  p.summary = 'A tool for packaging Ruby gems.'
+  p.url = 'http://blog.evanweaver.com/pages/code#echoe'
+  p.docs_host = 'blog.evanweaver.com:~/www/snax/public/files/doc/'
   p.dependencies = ['rake', 'rubyforge >= 0.4.0', 'highline']
 end
 
--- lib/echoe.rb
+++ lib/echoe.rb
@@ -18,11 +18,11 @@ Echoe includes some optional accessors for more advanced gem configuration.
 For example, Echoe's own <tt>Rakefile</tt> looks like this:
 
   Echoe.new('echoe') do |p|
-    p.rubyforge_name = "fauna"
-    p.author = "Evan Weaver"
-    p.summary = "A tool for packaging Ruby gems."
-    p.url = "http://blog.evanweaver.com/pages/code#echoe"
-    p.docs_host = "blog.evanweaver.com:~/www/snax/public/files/doc/"
+    p.project = 'fauna'
+    p.author = 'Evan Weaver'
+    p.summary = 'A tool for packaging Ruby gems.'
+    p.url = 'http://blog.evanweaver.com/pages/code#echoe'
+    p.docs_host = 'blog.evanweaver.com:~/www/snax/public/files/doc/'
     p.dependencies = ['rake', 'rubyforge >= 0.4.0', 'highline']
   end
 
